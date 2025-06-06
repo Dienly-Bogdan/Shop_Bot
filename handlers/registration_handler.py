bb6c9a3d74d8d6f910f85056254af686b695ea68
Dienly-Bogdan <bkulinichlol@yandex.ru>
2025-06-06 07:14:39 +0300

    Исправлен баг в handler, импортировал comand

--- handlers/registration_handler.py
+++ handlers/registration_handler.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
+from aiogram.filters import Command, StateFilter
 
 # from model import ClientTable  # раскомментируйте при наличии моделей
 
